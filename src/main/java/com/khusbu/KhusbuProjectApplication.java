b970af15b7cfdb3a8b839160b7f7d1e71c96c386
Rupak <rupak.chaulagain@cogenthealth.com.np>
2022-06-14 11:40:04 +0545

    project foudnation on spring boot

---
+++ src/main/java/com/khusbu/KhusbuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.khusbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhusbuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhusbuProjectApplication.class, args);
+    }
+
+}
